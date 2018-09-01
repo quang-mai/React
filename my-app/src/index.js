38184e1f3fd6c6c6d88c4200ac5936a696af350a
Quang Mai <quangmai09@gmail.com>
2018-09-01 00:53:01 -0500

    Add logic to determine winner

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -57,8 +57,15 @@ class Board extends React.Component {
 }
 
   render() {
-    const status = 'Next player: ' + (this.state.isX ? 'X' : 'O');
-
+    // const status = 'Next player: ' + (this.state.isX ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;      
+    } else {
+      status = 'Next player: ' +(this.state.isX ? 'X' : 'O');
+    }
+    
     return (
       <div>
         <div className="status">{status}</div>
@@ -96,6 +103,28 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares){
+  const winningCombinations = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ];
+  for (let i = 0; i < winningCombinations.length; i++){
+    const [a,b,c] = winningCombinations[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }
+    return null;
+  }
+
+
 }
 
 // ========================================
