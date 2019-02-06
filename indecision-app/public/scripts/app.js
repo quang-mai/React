9fca9608d00d963249c1fd1e9fc62affeceab81c
AdminQMai <AdminQMai@ufcunet.ad>
2019-02-06 17:40:56 -0600

    section 3 lecture 8

---
+++ indecision-app/public/scripts/app.js
@@ -0,0 +1,9 @@
+console.log ('App.js is running!');
+
+// JSX - JAvascript XML
+
+//var template = <p>This is JSX from app.js!</p>;
+var template = React.createElement("p", null, "This is JSX from app.js!");
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template,appRoot);
