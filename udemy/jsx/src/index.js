fb85fe6d01605f5d0cd8cbacd22a320b01aa1ca0
Quang Mai <quangmai09@gmail.com>
2019-09-03 20:19:51 -0500

    Section 1-12

--- udemy/jsx/src/index.js
+++ udemy/jsx/src/index.js
@@ -1,7 +1,14 @@
 // Import the React and ReactDOM libraries
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 // Create a react component
+const App = () => {
+  return <div>Hi there!</div>;
+};
 
 // Take the react component and show it on the screen
+ReactDOM.render(
+  <App />, //component created earlier
+  document.querySelector("#root") //inside index.html, get the reference of the div with id of 'root'
+);
