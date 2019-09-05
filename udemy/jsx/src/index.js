fb59dbdf8d659a6837092c3701094791641d0144
Quang Mai <quangmai09@gmail.com>
2019-09-04 23:04:24 -0500

    2.21: Values JSX Can't Show

--- udemy/jsx/src/index.js
+++ udemy/jsx/src/index.js
@@ -2,13 +2,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function getButtonText() {
-  return "Click on me";
-}
-
 // Create a react component
 const App = () => {
-  const buttonText = "Click Me!";
+  // const buttonText = "Click Me!"; //works!
+  // const buttonText = 12345;      //works
+  // const buttonText = ["Hi", "There"]; //works
+  // const buttonText = ["Hi", "There"]; //works!
+  const buttonText = { text: "Click me" }; //does not work as React child; must use buttonText.text
 
   return (
     <div>
@@ -18,8 +18,7 @@ const App = () => {
       <input id="name" type="text" />
       {/* <button style="background-color: blue; color: white;">Submit</button> */}
       <button style={{ backgroundColor: "blue", color: "white" }}>
-        {getButtonText()}
-        {/* {buttonText} */}
+        {buttonText.text}
       </button>
     </div>
   );
