7e0a20e687a64f8d472dbd20e62ff2cb48c4182a
Quang Mai <quangmai09@gmail.com>
2019-09-09 22:18:08 -0500

    3.39 Custom Component as a child

--- udemy/components/src/index.js
+++ udemy/components/src/index.js
@@ -2,28 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        time="Today at 6:00pm"
-        content="Nice blog post!"
-        avatar={Faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        time="Yesterday at 3:00pm"
-        content="Cool!"
-        avatar={Faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Alex"
-        time="Yesterday at 5:00"
-        content="Awesome!"
-        avatar={Faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          time="Today at 6:00pm"
+          content="Nice blog post!"
+          avatar={Faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          time="Yesterday at 3:00pm"
+          content="Cool!"
+          avatar={Faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          time="Yesterday at 5:00"
+          content="Awesome!"
+          avatar={Faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
